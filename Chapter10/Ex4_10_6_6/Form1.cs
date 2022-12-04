01160367f8f194dc04ff34a851ef7babc9fa2eac
bong <qhdwn25@gmail.com>
2022-12-04 18:34:06 +0900

    10.6(6) test

---
+++ Chapter10/Ex4_10_6_6/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex4_10_6_6
+{
+    public partial class Form1 : Form
+    {
+        String title = "누른 키";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Alt)
+                MessageBox.Show(e.KeyCode + " + Alt", title);
+            else if (e.Control)
+                MessageBox.Show(e.KeyCode + " + Control", title);
+            else if (e.Shift)
+                MessageBox.Show(e.KeyCode + " + Shift", title);
+            else
+                MessageBox.Show(e.KeyCode.ToString(), title);
+        }
+    }
+}
