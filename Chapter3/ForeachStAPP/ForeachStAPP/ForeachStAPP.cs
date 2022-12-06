1465e9d84758403ecfd941592ecfb070024788b1
bong <qhdwn25@gmail.com>
2022-12-06 13:25:54 +0900

    ForeachStAPP(1)

---
+++ Chapter3/ForeachStAPP/ForeachStAPP/ForeachStAPP.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class ForeachStAPP
+{
+    public static void Main()
+    {
+        string[] color = { "red", "green", "blue" };
+        foreach (string s in color)
+            onsole.WriteLine(s);
+    }
+}
