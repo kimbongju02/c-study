4176ab63b8165b704f1787435ca10c93fc0085b9
bong <qhdwn25@gmail.com>
2022-12-06 13:27:50 +0900

    IfElseStApp

---
+++ Chapter3/IfElseStApp/IfElseStApp/IfElseStApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class IfElseStApp
+{
+    public static void Main()
+    {
+        int n;
+        Console.Write("Enter a number = ");
+        n = Conole.Read() - '0';
+        if (n % 2 == 0)
+            Console.WriteLine(n + " is an even number.");
+        else
+            Console.WriteLine(n + " is an odd number.");
+    }
+}
