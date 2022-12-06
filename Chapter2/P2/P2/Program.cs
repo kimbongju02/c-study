ff6d908ae00d3db4e117deeaca790e3364cabb19
bong <qhdwn25@gmail.com>
2022-12-06 12:16:28 +0900

    2_2

---
+++ Chapter2/P2/P2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter2
+{
+    internal class program2
+    {
+        static void main(string[] args)
+        {
+            double f, c;
+            Console.Write("Write Celsies : ");
+            c=Console.Read();
+            f=9/5*c+32;
+            Console.WriteLine("Fahrenherit = ", f);
+        }
+    }
+}
