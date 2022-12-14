74ec34f7b0c42f46a39fca457f13f7daadc06dd5
bong <qhdwn25@gmail.com>
2022-12-14 11:06:16 +0900

    problem12_5_3(2)

--- Chapter12/Problem 12_5_3/Problem 12_5_3/Form1.cs
+++ Chapter12/Problem 12_5_3/Problem 12_5_3/Form1.cs
@@ -12,49 +12,54 @@ namespace Problem_12_5_3
 {
     public partial class Form1 : Form
     {
-        Rectangle rectMouse;
-        private Bitmap img, rectBmp;
-        private Rectangle zoomImgRect, rectRect;
+        private Rectangle rb, zoomImgRect, rectMouse;
+        Image img = new Bitmap(@"C:\Users\kimbo\OneDrive\바탕 화면\2-2수업\비주얼프로그래밍\code\Chapter12\Problem12_5_1\Problem12_5_1\석양.jpg");
 
         public Form1()
         {
             InitializeComponent();
-
-            Bitmap origin = new Bitmap("C: \\Users\\kimbo\\OneDrive\\바탕 화면\\2-2수업\\비주얼프로그래밍\\code\\Chapter12\\석양.jpg");
-            img = new Bitmap(origin, 800, 450);
+             zoomImgRect = new Rectangle(0,0, 800, 450); // 원본의 부분적인 크기
+             rb = new Rectangle(0, 0, 800, 450);
         }
 
-        private void pictureBox1_Paint(object sender, PaintEventArgs e, MouseEventArgs f)
+        private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            if(f.Button == MouseButtons.Left)
-            {
-                e.Graphics.DrawRectangle(Pens.Black, rectMouse);
-                e.Graphics.DrawImage(img, zoomImgRect);
-            }
+            e.Graphics.DrawImage(img, rb, zoomImgRect, GraphicsUnit.Pixel);
         }
 
-        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left)
-            {
-                rectMouse.X = e.X;
-                rectMouse.Y = e.Y;
-                rectMouse.Width = 0;
-                rectMouse.Height = 0;
-                pictureBox1.Invalidate();
-            }
-        }
 
-        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
         {
+            Graphics g = CreateGraphics();
             if (e.Button == MouseButtons.Left)
             {
                 rectMouse.Width = e.X - rectMouse.X + 1; // 사각형 width 계산
                 rectMouse.Height = e.Y - rectMouse.Y + 1; // 사각형 height 계산
-                zoomImgRect.X = -2 * (e.X - 75);
-                zoomImgRect.Y = -2 * (e.Y - 75);
-                pictureBox1.Invalidate();
+                zoomImgRect.Width = rectMouse.Width;
+                zoomImgRect.Height = rectMouse.Height;
+                g.DrawRectangle(Pens.Black, rectMouse);
             }
+            else
+            {
+                zoomImgRect = new Rectangle(0, 0, 800, 450);
+            }
+                Invalidate();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left) {
+                rectMouse.X = e.X;
+                rectMouse.Y = e.Y;
+                rectMouse.Width = 0;
+                rectMouse.Height = 0;
+                zoomImgRect.X = rectMouse.X;
+                zoomImgRect.Y = rectMouse.Y;
+                zoomImgRect.Width = 0;
+                zoomImgRect.Height = 0;
+            }
+
         }
     }
 }
