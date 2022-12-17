643a6c192cfea4f6a9d789bbddb202e9685ce9a2
bong <qhdwn25@gmail.com>
2022-12-17 17:07:04 +0900

    chapter10_3(3)

--- Chapter10/Problem3/Problem3/Form1.cs
+++ Chapter10/Problem3/Problem3/Form1.cs
@@ -27,12 +27,25 @@ namespace Ex3_10_6_3
             Invalidate(); // WM_PAINT 메시지 발생시킴
         }
 
-
-
-        private void Form1_Paint(object sender, PaintEventArgs e)
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
-            e.Graphics.DrawRectangle(Pens.Black, rectMouse);
+            if (e.Button == MouseButtons.Left)
+            {
+                if (e.X>rectMouse.X)
+                {
+                    rectMouse.Width = e.X - rectMouse.X + 1; // 사각형 width 계산
+                    rectMouse.Height = e.Y - rectMouse.Y + 1; // 사각형 height 계산
+                }
+                else
+                {
+                    rectMouse.Width = rectMouse.X - e.X + 1; // 사각형 width 계산
+                    rectMouse.Height = rectMouse.Y - e.Y + 1; // 사각형 height 계산
+                }
+                Invalidate(); // WM_PAINT 메시지 발생시킴
+            }
         }
+
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
