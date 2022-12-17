f80bfb8bfd664e620cd54dda6273533f7f071674
bong <qhdwn25@gmail.com>
2022-12-17 17:28:13 +0900

    MiddleTest2(1)

---
+++ MiddleTest/MiddleTest2/MiddleTest2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace midT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = args[0];
+            double b = double.Parse(args[1]);
+            double c = double.Parse(args[2]);
+
+
+
+        }
+    }
+}
