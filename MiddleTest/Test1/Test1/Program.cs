46494d050e13ce0b6efbf1dc90067d4526967549
bong <qhdwn25@gmail.com>
2022-12-17 17:26:26 +0900

    MiddleTest1(4)

--- MiddleTest/Test1/Test1/Program.cs
+++ MiddleTest/Test1/Test1/Program.cs
@@ -20,6 +20,10 @@ namespace MiddleTest
     {
         public double radius;
         public double ar;
+        public void CalArea()// 면적 프로퍼티를 이용해서 면적 저장
+        {
+            Ar=Math.PI*radius*radius;
+        }
 
     }
 }
