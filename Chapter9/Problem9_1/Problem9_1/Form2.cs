2da767edf79c570c37d00cb7221d1046cbb4df36
bong <qhdwn25@gmail.com>
2022-12-17 16:59:29 +0900

    Chapter9_1(4)

--- Chapter9/Problem9_1/Problem9_1/Form2.cs
+++ Chapter9/Problem9_1/Problem9_1/Form2.cs
@@ -17,6 +17,9 @@ namespace problem9_6_3
             InitializeComponent();
         }
 
-
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
