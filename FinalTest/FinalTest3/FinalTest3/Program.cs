7fd73403a865a1534363790bfe39c06c193c125d
bong <qhdwn25@gmail.com>
2022-12-17 17:16:15 +0900

    FinalTest2(1)

---
+++ FinalTest/FinalTest3/FinalTest3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Problem2
+{
+    class ThreadApp
+    {
+        private int[] rnd_data = new int[4];
+        private int prod;
+       
+}
