b5d86af31fcf12a6f405c265da78bfc5cfef1d93
bong <qhdwn25@gmail.com>
2022-12-17 17:14:27 +0900

    FinalTest1(2)

--- FinalTest/Problem2/Problem2/Program.cs
+++ FinalTest/Problem2/Problem2/Program.cs
@@ -8,14 +8,21 @@ namespace Problem1
 {
     internal class Program
     {
-
+        class CONN<T>
+        {
+            public string Connent(int OP,T a, T b)
+            {
+                string result=null;
+                if (OP== 0)
+                    result = a.ToString()+b.ToString();
+                else if (OP== 1)
+                    result =  b.ToString()+a.ToString();
+                return result;
+            }
+        }
         static void Main(string[] args)
         {
-            int OP;
-            int int_indata_1, int_indata_2;
-            double double_indata_1, double_indata_2;
-            CONN<int> intConn = new CONN<int>();
-            CONN<double> doubleConn = new CONN<double>();
+ouble>();
 
             Console.Write("Read OP from keyboard ? : ");
             OP = int.Parse(Console.ReadLine());
