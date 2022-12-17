7edfdaf56ec6c8e1920ead629582427cf265c6f1
bong <qhdwn25@gmail.com>
2022-12-17 17:08:14 +0900

    Chapter10_4(1)

---
+++ Chapter10/Problem10_4/Problem10_4/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex4_10_6_6
+{
+    public partial class Form1 : Form
+    {
+        String title = "누른 키";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
