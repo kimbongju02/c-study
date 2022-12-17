e816f2d78811ed25693584c22042e113e44b6951
bong <qhdwn25@gmail.com>
2022-12-17 17:01:27 +0900

    Chapter9_3(1)

---
+++ Chapter9/Problem9_3/Problem9_3/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace problem9_6_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        {
+            if (checkBox1.Checked==true &&checkBox2.Checked==false)
+            {
+                fontDialog1.ShowColor=false; //font color change combobex delete
+                fontDialog1.ShowApply=true; //apply button create
+                fontDialog1.ShowDialog(); //font dialog box create
+            }
+            else if (checkBox1.Checked==false &&checkBox2.Checked==true)
+            {
+                fontDialog1.ShowColor=true; //font color change combobex add
+                fontDialog1.ShowApply=false; //apply button delete
+                fontDialog1.ShowDialog(); //font dialog box create
+            }
+            else if (checkBox1.Checked==true && checkBox2.Checked==true)
+            {
+                fontDialog1.ShowColor=true; //font color change add
+                fontDialog1.ShowApply=true; //apply button create
+                fontDialog1.ShowDialog(); //font dialog box create
+            }
+            else
+            {
+                fontDialog1.ShowColor=false; //font color change combobex delete
+                fontDialog1.ShowApply=false; //apply button delete
+                fontDialog1.ShowDialog(); //font dialog box create
+            }
+        }
+    }
+}
